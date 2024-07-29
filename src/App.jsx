11141f6cc9db73c44f68385fabcfbc210cc2c8e2
lester <lesterc013@gmail.com>
2024-07-29 18:05:18 +0800

    Implement GET using React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './request'
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1,
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data
+
+  if (result.isLoading) {
+    return <div>Fetching data...</div>
+  } else if (result.isError) {
+    return <div>Error: {result.error.message}</div>
+  }
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
